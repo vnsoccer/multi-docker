d03b3ba884af785dd84bdd7c6c41086817e66c60
vnsoccer <vnsoccer00@gmail.com>
2019-07-01 20:29:16 +0700

    turn back client

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import Fib from './Fib';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Fibonacci Calculator</h1>
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other Page</Link>
+        </header>
+
+        <div>
+          <Route exact path="/" component={Fib} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
